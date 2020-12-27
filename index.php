420ae0793f16a4af9d55a08606e1a5305a96180c
Mirko-T <mirkotomic3@gmail.com>
2020-12-27 17:10:53 +0100

    add conditional statment

--- index.php
+++ index.php
@@ -8,17 +8,25 @@
     $name = "PHP Store";
     $credit = 1000;
 
-    echo "<h1> Welcome to: " .$name. "!</h1>";
-    echo "<h2> You have $" .$credit. " in your wallet.</h2>";
+      echo "<h1> Welcome to: " .$name. "!</h1>";
+      echo "<h2> You have $" .$credit. " in your wallet.</h2>";
 
-      $products['Computer'] = 750;
-      $products['Car'] = 15000;
-      $products['iPhone'] = 1000;
-      $products['Toster'] = 75;
+        $products['Computer'] = 750;
+        $products['Car'] = 15000;
+        $products['iPhone'] = 1000;
+        $products['Toster'] = 75;
 
-      foreach($products as $key => $value){
-        echo "<p> The ".$key." costs ".$value.".</p>";
-      }
+        foreach($products as $key => $value){
+          echo "<p> The ".$key." costs ".$value.".</p>";
+        }
+
+        echo "<h2> Items you can afford </h2>";
+        foreach($products as $key => $value){
+          if($value <= $credit) {
+            echo "<p> ".$key."</p>";
+          }
+        }
+          
     ?>
   </body>
 </html>
